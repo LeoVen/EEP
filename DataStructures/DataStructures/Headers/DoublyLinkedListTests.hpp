26ad48ad31809696585409cbfecd077a1b8bc320
LeoVen <lvenk26@gmail.com>
2018-03-14 14:13:18 -0300

    Data Structures moved
    Now about to share both C and C++ code so it was removed from C++ folder

---
+++ DataStructures/DataStructures/Headers/DoublyLinkedListTests.hpp
@@ -0,0 +1,13 @@
+/*
+* DoublyLinkedListTests.hpp
+*
+* Author: Leonardo Vencovsky
+* Created on 13/03/2018
+*
+* Header for Doubly Linked Lists Tests
+*
+*/
+
+#pragma once
+
+int DoublyLinkedListTests();
---
+++ DataStructures/DataStructures/Headers/SinglyLinkedListTests.hpp
@@ -0,0 +1,13 @@
+/*
+* SinglyLinkedListTests.hpp
+*
+* Author: Leonardo Vencovsky
+* Created on 13/03/2018
+*
+* Header for Singly Linked Lists Tests
+*
+*/
+
+#pragma once
+
+int SinglyLinkedListTest();
