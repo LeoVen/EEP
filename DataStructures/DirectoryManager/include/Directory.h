c722eb035e54ec8b6e085e10ceec064bfa6a5110
Leonardo <lvenk26@gmail.com>
2018-03-27 22:35:38 -0300

    Directory Manager Initial Commit

--- DataStructures/DirectoryManager/include/Directory.h
+++ DataStructures/DirectoryManager/include/Directory.h
@@ -15,16 +15,35 @@
 *
 */
 
-#ifndef DIRECTORY_H
-#define DIRECTORY_H
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-typedef struct Directory {
-	char *name;
-} Directory;
+#define MAX_DIRNAME_SIZE 20
 
-Directory * getDirectoryByName(char *name);
 
+	typedef struct Directory {
+		char *name;
+		struct Directory *parent;
+		struct Directory *list;
+		struct Directory *next;
+	} Directory;
+
+	Directory * getDirectoryByName(char *name);
+
+	void changeDirName(Directory *dir, char *name);
+
+	int makeDirectory(Directory **current, char *name);
+	
+	int listDirectory(Directory **current);
+
+#ifdef __cplusplus
+}
 #endif
--- DataStructures/DirectoryManager/src/Directory.c
+++ DataStructures/DirectoryManager/src/Directory.c
@@ -20,7 +20,43 @@
 Directory * getDirectoryByName(char *name)
 {
 	Directory *dir = (Directory *)malloc(sizeof(Directory));
-	dir->name = " ";
-	dir->name = name;
+	dir->parent = NULL;
+	dir->list = NULL;
+	dir->next = NULL;
+	changeDirName(dir, name);
 	return dir;
+}
+
+void changeDirName(Directory *dir, char *name)
+{
+	dir->name = (char *)malloc(sizeof(char) * strlen(name) + 1);
+	int i;
+	for (i = 0; i <= strlen(name); i++) {
+		dir->name[i] = name[i];
+	}
+}
+
+int makeDirectory(Directory **current, char *name)
+{
+	Directory *curr = *current;
+	Directory *newDir = (Directory *)malloc(sizeof(Directory));
+	changeDirName(newDir, name);
+	newDir->list = NULL;
+	newDir->parent = curr;
+	newDir->next = curr->list;
+	curr->list = newDir;
+}
+
+int listDirectory(Directory **current)
+{
+	Directory *curr = *current;
+	Directory *head = curr->list;
+	printf("\n");
+	while (head != NULL)
+	{
+		printf("\t%s", head->name);
+		head = head->next;
+	}
+	printf("\n");
+	return 0;
 }
