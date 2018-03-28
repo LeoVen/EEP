74c5a02abdd8cb3c835110004ab63568e7a821b9
Leonardo <lvenk26@gmail.com>
2018-03-27 23:16:36 -0300

    Update source file headers

--- DataStructures/DirectoryManager/include/Directory.h
+++ DataStructures/DirectoryManager/include/Directory.h
@@ -4,7 +4,9 @@
 * EEP
 *
 * Created on 27/03/2018
-* by Leonardo Vencovsky
+* by Leonardo Vencovsky 201710276
+* by Guilherme Pinazza  201710351
+* by Vitor Facioli      201710292
 *
 * Last modified on 27/03/2018
 * by Leonardo Vencovsky
