4ed98713039c499462fa8c888908015fd0168196
Leonardo Vencovsky <lvenk26@gmail.com>
2018-05-08 20:36:25 -0300

    Update processo_pai_02.c

--- C/2018/Sistemas Operacionais/processo_pai_02.c
+++ C/2018/Sistemas Operacionais/processo_pai_02.c
@@ -4,15 +4,27 @@
 
 int main(int argc, char const *argv[])
 {
+	pid_t var;
 	system("clear");
 
 	printf("Processo-Pai: Iniciando a execucao.\n");
-	fork();
-	fork();
-	printf("Processo-Filho: Iniciando a execucao\n");
-	sleep(5);
-	printf("Processo-Filho: Encerrando a execucao\n");
-	printf("Processo-Pai: Encerrando a execucao\n");
+
+	// Var recebe o PID do processo filho
+	// É retornado 0 (para o processo pai) quando o fork() retorna
+	var = fork();
+
+	if (var == 0) {
+		printf("Processo-Filho: Iniciando a execucao\n");
+		sleep(10);
+		printf("Processo-Filho: Encerrando a execucao\n");
+	}
+	else {
+		sleep(5);
+		printf("Processo-Pai rodou Processo-Filho: %d\n", var);
+		printf("Processo-Pai: Encerrando a execucao\n");
+	}
+
+	// Note nos sleep() : processos filhos podem continuar existindo sem o pai
 
 	return 0;
 }
