acb4b684175911b31bd9d9908bb6b996bdf47bba
Leonardo Vencovsky <lvenk26@gmail.com>
2018-12-01 11:51:13 -0200

    Update DBConn.cs

--- SQL/MS SQL/DBConn.cs
+++ SQL/MS SQL/DBConn.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Data.SqlClient;
 
 namespace DBConnection
@@ -7,7 +7,8 @@ namespace DBConnection
     {
         static void Main(string[] args)
         {
-            using (var conn = new SqlConnection("Server=LDC3-24;Database=NORTHWND;User Id=sa;Password=12345678;"))
+            using (var conn = new SqlConnection("Server=LDC3-24;Database=NORTHWND;" +
+                "User Id=sa;Password=12345678;"))
             {
                 try
                 {
@@ -15,16 +16,40 @@ namespace DBConnection
 
                     Console.WriteLine("Conexão com sucesso");
 
-                    var command = new SqlCommand("select * from Products;");
+                    var command = new SqlCommand("SELECT " +
+                        "ProductId, ProductName, UnitPrice, " +
+                        "( SELECT MAX(o.UnitPrice) FROM [Order Details] o" +
+                        " WHERE o.ProductID = p.ProductId ) as MaxUnitPrice" +
+                        " FROM Products p;");
 
                     command.Connection = conn;
 
                     SqlDataReader reader = command.ExecuteReader();
 
+                    PrintDivider();
+
+                    Console.WriteLine(String.Format(
+                            "| {0,10} | {1,40} | {2,15} | {3,15} |",
+                            "ProductId",
+                            "ProductName",
+                            "UnitPrice",
+                            "MaxUnitPrice")
+                    );
+
                     while (reader.Read())
                     {
-                        Console.WriteLine($"{reader["ProductId"]} {reader["ProductName"]}");
+
+                        PrintDivider();
+
+                        Console.WriteLine(String.Format(
+                            "| {0,10} | {1,40} | {2,15:C2} | {3,15:C2} |",
+                            reader["ProductId"],
+                            reader["ProductName"],
+                            reader["UnitPrice"],
+                            reader["MaxUnitPrice"]));
                     }
+
+                    PrintDivider();
                 }
                 catch (SqlException e)
                 {
@@ -39,6 +64,14 @@ namespace DBConnection
             }
             
             Console.ReadKey();
+        }
+
+        static void PrintDivider()
+        {
+            Console.WriteLine("+------------+" +
+                                "------------------------------------------+" +
+                                "-----------------+" +
+                                "-----------------+");
         }
     }
 }
