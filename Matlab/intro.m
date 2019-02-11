9f0eae00a5369b5ed1f7333c7f8d3e0d78e8b58c
Leonardo Vencovsky <lvenk26@gmail.com>
2019-02-11 20:42:57 -0200

    Create intro.m

---
+++ Matlab/intro.m
@@ -0,0 +1,86 @@
+% Comment
+
+% Clears console
+clc
+
+% Columns
+% [1 2 3]
+m1 = [1, 2, 3];
+
+% Rows
+% [1]
+% [2]
+% [3]
+m2 = [3; 2; 1];
+
+% [1 2 3]
+% [4 5 6]
+% [7 8 9]
+m3 = [[1, 2, 3];[4, 5, 6];[7, 8, 9]];
+
+% Complex
+c1 = 2-5i;
+
+% Previous answer
+ans;
+
+% Infinite
+Inf
+
+% Not a number
+NaN
+
+% Pi
+pi
+
+% Floating point relative accuracy
+eps
+
+% Biggest floating number
+realmax
+% Smallest floating number
+realmin
+
+% Largest consecutive integer in floatin-point format
+flintmax
+
+% Biggest Integer
+intmax
+% Smallest Integer
+intmin
+
+% Sum
+% +
+
+% Subtraction
+% -
+
+% Matrix Multiplication
+% *
+
+% Array Multiplication
+% .*
+
+% Matrix Right Division
+% /
+
+% Array Right Division
+% ./
+
+% Matrix Left Division
+% \
+
+% Array Left Division
+% .\
+
+% Matrix Power
+% ^
+
+% Array power
+% .^
+
+% Matrix Transpose
+% '
+
+% Array Transpose
+% .'
